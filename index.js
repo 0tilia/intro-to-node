fecbd5ccc7ed8ebcf3dc6c5ad67fbb477da59933
Otilia Toma <otilia@Otilias-MacBook-Air.local>
2023-03-15 21:14:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var superheroes = require("superheroes");
+
+var mySuperheroName = superheroes.random();
+
+console.log(mySuperheroName);
+
+var supervillains = require("supervillains");
+
+var mySuperVillain = supervillains.random();
+
+console.log(mySuperVillain);
+
+
